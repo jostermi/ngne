13c701811f19a1e0eb5830a9210a4fa98ecf150c
cobedien <cobedien@cisco.com>
2015-06-17 10:23:42 -0400

    Create show_version.py

---
+++ show_version.py
@@ -0,0 +1,28 @@
+__author__ = 'cobedien'
+
+
+import sys
+import json
+import requests
+
+my_headers = {'content-type': 'application/json-rpc'}
+url = "http://10.87.6.210/ins"
+username = "admin"
+password = "Cisco321"
+
+
+payload = [{'jsonrpc': '2.0', 'method': 'cli', 'params': ['show version',1], 'id': '1'}]
+my_data = json.dumps(payload)
+response = requests.post(url, data=my_data, headers=my_headers, auth=(username, password))
+
+
+kick_start_image = response.json()['result']['body']['kickstart_ver_str']
+system_image = response.json()['result']['body']['kick_file_name']
+host_name = response.json()['result']['body']['host_name']
+
+print ("")
+print ("===============================")
+print ('host name:'+ host_name)
+print ('kickstart image version :' + kick_start_image)
+print ('system image version :s' + system_image)
+print ("===============================")
